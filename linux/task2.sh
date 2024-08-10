7e3ab20eba9ce6e8454357336a60b7057925972f
Raghavendra DS <raghavendrasak78@gmail.com>
2024-08-10 21:59:46 +0530

    completed task2.sh

--- linux/task2.sh
+++ linux/task2.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo "script for the Accept username & password"
-echo "********************"
-echo "password must be "upper+lower case""
+echo "Enter the file name: "
+read FILE_NAME
 
-read -p "Enter username: " username
-read -sp "Enter password: " password
+if [ -f "$FILE_NAME" ];
+      then
+	      echo "Number of lines : $(wc -l < "$FILE_NAME" ) "
 
-if [[ $password =~ ^[a-zA-Z]+$ ]];
-then
-     echo "Password is valid"
-else
-   echo "password must be "upper+lower letters""
-   
-fi
+	      echo "Number of words: $(wc -w < "$FILE_NAME" ) "
+
+	      echo "Number of charc: $(wc -c < "$FILE_NAME" ) "
+
+      else 
+              echo "file is not found "
+
+fi	      
+	     
