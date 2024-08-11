30a0b42d0c32ef73df27b9e85071c11d71d3b310
Navanath <biradarnavanath1@gmail.com>
2024-08-11 17:25:07 +0530

    file task01.sh updated

--- linux/task01.sh
+++ linux/task01.sh
@@ -6,3 +6,8 @@ echo "Memory: $(free -h | awk 'NR==2 {print $2,$3,$4}')"
 echo "CPU: $(uname -p)"
 echo "Disk: $(df -h / | awk 'NR==2 {print $4}')"
 
+# CPU utilization
+
+ps -eo tid,pid,pcpu | sort -k 8 -r | head -10
+
+
