710adf815d160dd243cf661c5f91d75eaae87d7a
Navanath <biradarnavanath1@gmail.com>
2024-08-11 18:55:00 +0530

    created task1 1st one

---
+++ linux/task1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+############
+# Athour= Navanath 
+#date = 11 aug 24
+############
+
+find /path/to/search -name "*.java" -size +10M -mtime +10 -print
+
+find /var -name "*.log" -print
