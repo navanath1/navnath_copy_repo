ac555eaf5fe8b4ba92ccae0bc2ebab4d21605161
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-11 18:38:46 +0530

    Created jtask1.groovy
    for jenkins job

---
+++ jenkins/jtask1.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Server Details') {
+            steps {
+                script {
+                    echo ">>>>> Details of hostname, memory, CPU, disk <<<<<<<<<<<"
+
+                    sh '''
+                        name=$(uname -n)
+                        mem=$(free -h --total | awk '/^Total/ {print $1, $2}')
+                        cpu=$(mpstat -T | awk 'NR==4 {print "CPU: " $3}')
+                        dis=$(df -a -h --total | awk '/^total/ {print $1 ":" $2}')
+                        
+                        echo ">>>>>>>>>>>>>>> HOST NAME <<<<<<<<<<<<<<<<"
+                        echo "$name"
+                        echo ">>>>>>>>>>>> MEMORY DETAILS <<<<<<<<<<<<<<<<"
+                        echo "$mem"
+                        echo ">>>>>>>>>>>> CPU DETAILS <<<<<<<<<<<<<<<<<<<"
+                        echo "$cpu"
+                        echo ">>>>>>>>>>>> DISK DETAILS <<<<<<<<<<<<<<<<<<<"
+                        echo "$dis"
+                    '''
+                }
+            }
+        }
+    }
+}
