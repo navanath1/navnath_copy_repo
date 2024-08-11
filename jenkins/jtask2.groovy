ccf37597c4ca6bbbfba29675129e011276da764c
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-11 20:04:48 +0530

    forgot grovvy stage "all" script hence >>"updated"<<

--- jenkins/jtask2.groovy
+++ jenkins/jtask2.groovy
@@ -1,1 +1,16 @@
-sh 'aws s3 cp  /var/lib/jenkins  s3://billx-server-bucket --recusrsive'
+pipeline {
+    agent any
+
+    stages {
+        stage('Server Details') {
+            steps {
+                script {
+                  sh 'aws s3 cp  /var/lib/jenkins  s3://billx-server-bucket --recusrsive'
+
+                  
+                }
+            }
+        }
+    }
+}
+
