45a5d5d2b10ba50727926fa43e4cc949113a1f17
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-11 20:29:37 +0530

    Update over s3 folder

--- jenkins/jtask2.groovy
+++ jenkins/jtask2.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('BACKUP') {
             steps {
                 script {
-                  sh 'aws s3 sync  /var/lib/jenkins/  s3://billx-server-bucket/ ' 
+                  sh 'aws s3 sync  /var/lib/jenkins/  s3://billx-server-bucket/Jenkins/ ' 
 
                   
                 }
