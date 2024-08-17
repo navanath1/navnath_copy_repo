14a18d6ed377474dea3f018088ca6a8e74dc65c3
pawannraj <98880956+pawannraj@users.noreply.github.com>
2024-08-17 11:46:25 +0530

    Update Task2a.groovy

--- jenkins/Task2a.groovy
+++ jenkins/Task2a.groovy
@@ -1,1 +1,12 @@
+pipeline {
+    agent any
+    stages {
+        stage('Backup jenkins server') {
+            steps {
+                tar -czvf jenkins-backup.tar.gz ${JENKINS_HOME}
+                aws s3 cp jenkins-backup.tar.gz s3://jenkinsfilesbackup
+            }
+        }
+    }
+}
 
