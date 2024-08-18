5622e0c1ad26b6b99eaefd9ea4aa63f2f5df67d4
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-18 21:07:03 +0530

    Update task4.groovy

--- jenkins/task4.groovy
+++ jenkins/task4.groovy
@@ -1,18 +1,16 @@
 pipeline {
     agent any
-    parameters {
-        string(name: 'STRING_PARAM', defaultValue: 'default', description: 'String to match')
-    }
+   
     stages {
         stage('String Matching Parameter') {
             steps {
                 script {
-                    // Access the Jenkins parameter
-                    def stringParam = env.STRING_PARAM
+                    // // Access the Jenkins parameter
+                    // def stringParam = env.STRING_PARAM
 
                     // Use correct shell syntax for string comparison
                     sh """
-                        if [ "${stringParam}" = "nitin" ]; then
+                        if [ env.string = "nitin" ]; then
                             echo "correct"
                         else
                             echo "wrong"
