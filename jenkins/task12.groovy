d3a9184a426643bf59df3e0975a80c3c81c935e0
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-18 23:31:28 +0530

    Update task12.groovy

--- jenkins/task12.groovy
+++ jenkins/task12.groovy
@@ -1,13 +1,100 @@
 pipeline {
-    agent any
+    agent none
 
     stages {
-        stage('Server Details') {
+        parallel{              ///////////////same from task 3 but using parallel to work at same time 
+        stage('m1') {
+            agent{label 'master'}
             steps {
                 script {
-                  sh 'SET n="job1"'
+                  sh '''echo "1 from master" 
+                        uname'''
                 }
             }
         }
+          stage('m2') {
+            agent{label 'master'}
+            steps {
+                script {
+                  sh '''echo "2 from master" 
+                        uname'''
+                }
+            }
+        }
+          
+          stage('m3') {
+            agent{label 'master'}
+            steps {
+                script {
+                  sh '''echo "3 from master" 
+                        uname'''
+                }
+            }
+        }
+          stage('m4') {
+            agent{label 'master'}
+            steps {
+                script {
+                  sh '''echo "4 from master" 
+                        uname'''
+                }
+            }
+        }
+          stage('m5') {
+            agent{label 'master'}
+            steps {
+                script {
+                  sh '''echo "5 from master" 
+                        uname'''
+                }
+            }
+        }
+          stage('s1') {
+            agent{label 'slave1'}
+            steps {
+                script {
+                  sh '''echo "1 from slave" 
+                        uname'''
+                }
+            }
+        }
+          stage('s2') {
+            agent{label 'slave1'}
+            steps {
+                script {
+                  sh '''echo "2 from slave" 
+                        uname'''
+                }
+            }
+        }
+          stage('s3') {
+            agent{label 'slave1'}
+            steps {
+                script {
+                  sh '''echo "3 from salve " 
+                        uname'''
+                }
+            }
+        }
+          stage('s4') {
+            agent{label 'slave1'}
+            steps {
+                script {
+                  sh '''echo "4 from salve " 
+                        uname'''
+                }
+            }
+        }
+          stage('s5') {
+            agent{label 'slave1'}
+            steps {
+                script {
+                  sh '''echo "5 from salve " 
+                        uname'''
+                }
+            }
+        }
+        }
+        
     }
 }
