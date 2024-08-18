3359ffe6acf72a587498ce1e87c78b59885247d8
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-18 22:35:51 +0530

    Update task6.groovy

--- jenkins/task6.groovy
+++ jenkins/task6.groovy
@@ -1,13 +1,24 @@
 pipeline {
     agent any
-
+    
     stages {
-        stage('Server Details') {
-            steps {
-                script {
-                  sh 'SET n="job1"'
+        try{
+            stage('try@stage1') {
+                steps {
+                    script {
+                      sh 'eco "nitin"'
+                    }
                 }
             }
         }
+        catch (Exception e) {
+            stage('catch@STAGE2') {
+                steps {
+                    script {
+                      sh 'echo "this show error above stage ${e.message}"'
+                    }
+                }
+            }
+             }
     }
 }
