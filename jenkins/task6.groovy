69a6f6c0bcdb1fccca359d18c5a3e4119b583677
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-18 22:47:50 +0530

    Update task6.groovy

--- jenkins/task6.groovy
+++ jenkins/task6.groovy
@@ -1,12 +1,38 @@
-node {
-    agent any
+// node {
+//     agent any
+    
+//     stages {
+//         try{
+//             stage('try@stage1') {
+//                 steps {
+//                     script {
+//                       sh 'eco "nitin"'
+//                     }
+//                 }
+//             }
+//         }
+//         catch (Exception e) {
+//             stage('catch@STAGE2') {
+//                 steps {
+//                     script {
+//                       sh 'echo "this show error above stage ${e.message}"'
+//                     }
+//                 }
+//             }
+//              }
+//     }
+// }
+// tried in shell not understanding trying in python
+
+pipeline {
+    agent {label 'master'}
     
     stages {
         try{
             stage('try@stage1') {
                 steps {
                     script {
-                      sh 'eco "nitin"'
+                      py'print("nitin")'
                     }
                 }
             }
@@ -15,8 +41,7 @@ node {
             stage('catch@STAGE2') {
                 steps {
                     script {
-                      sh 'echo "this show error above stage ${e.message}"'
-                    }
+                      py 'print(e)'
                 }
             }
              }
