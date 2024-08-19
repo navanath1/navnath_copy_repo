0b3ab3bed7cbea3fa9f4288ac782cd27ca52fec7
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-19 11:49:04 +0530

    Update task9_job2.groovy

--- jenkins/task9_job2.groovy
+++ jenkins/task9_job2.groovy
@@ -1,11 +1,11 @@
-opipeline {
+pipeline {
     agent any
-
+@task9_job1
     stages {
         stage('Server Details') {
             steps {
                 script {
-                  sh 'echo "this is job9b using varable job9a ${env.v1}"'
+                  sh 'echo "this is job9b using varable job9a ${job1}"'
                 }
             }
         }
