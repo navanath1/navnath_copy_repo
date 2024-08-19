8e7f4eca6b492b12e9a8017bd48fbdb8cf69a4f2
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-19 14:58:10 +0530

    Update task9_job2.groovy

--- jenkins/task9_job2.groovy
+++ jenkins/task9_job2.groovy
@@ -6,6 +6,7 @@ pipeline {
         stage('Server Details') {
             steps {
                 script {
+                    def job1 =share.task9.job1.getJob1Value()
                   sh 'echo "this is job9b using varable job9a ${job1}" '
                 }
             }
