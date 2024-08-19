766670b9aba0a8d95885757fbf8a278b1d28526d
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-19 12:41:46 +0530

    Update task9_job1.groovy

--- jenkins/task9_job1.groovy
+++ jenkins/task9_job1.groovy
@@ -3,9 +3,10 @@ pipeline {
 
     stages {
         stage('Server Details') {
-            job1="this is job1 variable"
+           
             steps {
                 script {
+                    job1="this is job1 variable"
                   sh ' echo "${job1}" '
                 }
             }
