8afad0520f3896e6e5817ffdd0cda1c2d422c7d7
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-19 19:50:42 +0530

    Update task11.groovy

--- jenkins/task11.groovy
+++ jenkins/task11.groovy
@@ -3,22 +3,23 @@ pipeline {
 
     stages {
         stage('1') {
+
             agent {label "slave1"}
+
             steps {
                 script {
-                    
                     try{
-                  sh 'eco "this fails " || echo "this is runs using ||"'}
+                        sh 'eco "this fails " || echo "this is runs using ||"'}
                     catch (e){ 
                         sh 'echo "$(e)"'
-                         stage('2') {
-                             agent{label ""master}
-            steps {
-                script {
-                  sh 'echo "this fails" '
-                }
-            }
-        }
+                        stage('2') {
+                            agent{label "master"}
+                            steps {
+                                script {
+                                sh 'echo "this is master becasuse slave fails" '
+                                }
+                            }
+                        }
                         
                     }
                   
