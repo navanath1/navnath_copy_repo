8842cb855410e3c66749529b9e1c7ea0b87b7f16
Nitinsai.Avirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2024-08-19 19:58:19 +0530

    Update task11.groovy

--- jenkins/task11.groovy
+++ jenkins/task11.groovy
@@ -1,33 +1,65 @@
+// pipeline {
+//     agent none
+
+//     stages {
+//         try{
+//             stage('1') {
+
+//                 agent {label "slave1"}
+
+//                 steps {
+//                     script {
+                        
+//                             sh 'eco "this fails " || echo "this is runs using ||"'
+//                     } 
+//                 }
+//             }
+//         }
+
+//         catch (e){ 
+//                 sh 'echo "$(e)"'
+//                 stage('2') {
+//                     agent{label "master"}
+//                     steps {
+//                         script {
+//                                 sh 'echo "this is master becasuse slave fails" '
+//                             }
+//                         }
+//                     }
+                        
+//                 }
+       
+//     }
+// }
+// this will not work beacuse try catch in direct declartive
+// three options
+// 1
 pipeline {
     agent none
 
     stages {
-        try{
-            stage('1') {
-
-                agent {label "slave1"}
-
-                steps {
-                    script {
-                        
-                            sh 'eco "this fails " || echo "this is runs using ||"'
-                    } 
+        stage('1') {
+            agent { label "slave1" }
+            steps {
+                script {
+                    // Intentionally causing a failure to test the catch
+                    sh 'echo "this fails " || echo "this runs due to ||"'
                 }
             }
         }
-
-        catch (e){ 
-                sh 'echo "$(e)"'
-                stage('2') {
-                    agent{label "master"}
-                    steps {
-                        script {
-                                sh 'echo "this is master becasuse slave fails" '
-                            }
-                        }
+    }
+    post {
+        failure {
+            // Handles failures in the previous stages
+            stage('2') {
+                agent { label "master" }
+                steps {
+                    script {
+                        sh 'echo "This is running on the master because slave1 failed"'
                     }
-                        
                 }
-       
+            }
+        }
     }
 }
+// 2
