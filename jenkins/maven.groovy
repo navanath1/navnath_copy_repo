158b0e992d46038a22d6f1fc43acf18920ac9964
Raghuds48 <raghavendrasak78@gmail.com>
2024-08-23 22:24:48 +0530

    Update maven.groovy

--- jenkins/maven.groovy
+++ jenkins/maven.groovy
@@ -5,10 +5,18 @@ pipeline {
         stage('Install Maven') {
             steps {
                 script {
-                                        sh 'sudo yum install -y maven'
-                   
-                      }
+                    if (!isMavenInstalled()) {
+                        sh 'sudo yum install -y maven'
+                        echo 'Maven installed successfully'
+                    } else {
+                        echo 'Maven already installed'
+                    }
                 }
             }
         }
+    }
+}
+
+def isMavenInstalled() {
+    return sh(script: 'mvn -v', returnStatus: true) == 0
 }
