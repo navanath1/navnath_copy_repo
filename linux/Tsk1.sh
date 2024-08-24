84cd6a5a49579e5800751ece94f813f1c5c9c4f8
Sunit-Nayak <131778342+Sunit-Nayak@users.noreply.github.com>
2024-08-25 00:08:50 +0530

    Create Tsk1.sh

---
+++ linux/Tsk1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo" print details of server"
+nameinfo=$(uname -a)
+memoryinfo=$(free -h)
+diskinfo=$(df -h)
+cpuInfo=$(lscpu)
+echo "1  Server name : $nameinfo"
+echo "2 memory information : $memoryinfo "
+echo "3 diskinfo : $diskinfo "
+echo " cpu Information : $cpuInfo "
