ee68b6a8683018233802d473370d89afac1dcd94
Raghuds48 <raghavendrasak78@gmail.com>
2024-08-26 17:17:44 +0530

    Update task10.groovy

--- jenkins/task10.groovy
+++ jenkins/task10.groovy
@@ -5,6 +5,18 @@ pipeline {
         stage('Create artifacts') {
             steps {
                 script {
+                     def offset = "-"
+
+                    // Get the current date and time in the desired format
+                    def dateTime = new Date().format("yyyy-MM-dd HH:mm:ss")
+
+                    // Check the current build result
+                    if (currentBuild.result == null || currentBuild.result == 'SUCCESS' ) {
+                        // Set the display name for a successful build
+                        currentBuild.displayName =  (currentBuild.number + offset) + "pass" + " - " + dateTime
+                    } else {
+                        // Set the display name for a failed build
+                        currentBuild.displayName =  (currentBuild.number + offset) + "fail" + " - " + dateTime
                    
                     sh 'echo "Hello World" > artifact.txt'
                     sh 'echo "This is another artifact" > artifact2.txt'
